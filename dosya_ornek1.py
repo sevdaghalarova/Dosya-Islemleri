451dc1a6e934b17793e6e2ef3780870af356d504
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-10 22:35:54 +0400

    Doysa islemleri ornek

--- dosya_ornek1.py
+++ dosya_ornek1.py
@@ -1,49 +1,28 @@
-def not_hesapla(satir):
-    satir=satir[:-1]
-    liste=satir.split(",")
-    isim=liste[0]
-    not1=int(liste[1])
-    not2=int(liste[2])
-    not3=int(liste[3])
-    son_not=(not1*3/10)+(not2*3/10)+(not3*4/10)
-    harf = ""
-    if son_not>=90:
-        harf="AA"
-    elif son_not>=80:
-        harf="BB"
-    elif son_not>=70:
-        harf="BC"
-    elif son_not>=60:
-        harf="CC"
-    elif son_not>=50:
-        harf="DD"
-    else:
-        harf="FF"
-    return isim+","+harf+"\n"
+with open("dosya.txt","r",encoding="utf-8") as file:
+    fb=[]
+    gs=[]
+    bsk=[]
+    for futbolcu in file:
+        futbolcu=futbolcu[:-1]
+        liste1=futbolcu.split(",")
+        isim=liste1[0]
+        klup=liste1[1]
+        if klup=="Galatasaray":
+            gs.append(futbolcu + "\n")
+        elif klup=="Fenerbahçe":
+            fb.append(futbolcu + "\n")
+        else:
+            bsk.append(futbolcu + "\n")
+        with open("gs.txt","w",encoding="utf-8") as gsaray:
+            for i in gs:
+                gsaray.write(i)
 
-with open ("dosya.txt","r",encoding="utf-8") as file:
-    eklenecekler_listesi=[]
-    for i in file:
-        eklenecekler_listesi.append(not_hesapla(i))
-    with open("notlar.txt","w",encoding="utf-8") as file1:
-        for i in eklenecekler_listesi:
-            file1.write(i)
-    with open("notlar.txt","r",encoding="utf-8") as file2:
-        kalanlar=[]
-        gecenler=[]
-        for i in file2:
-            i=i[:-1]
-            liste=i.split(",")
-            isim=liste[0]
-            harf=liste[1]
-            if harf=="FF":
-                kalanlar.append(i+"\n")
-            else:
-                gecenler.append(i+"\n")
+        with open("fb.txt","w",encoding="utf-8") as fbahce:
+            for i in fb:
+                fbahce.write(i)
 
-    with open("gecenler.txt","w",encoding="utf-8") as file3:
-        for i in gecenler:
-            file3.write(i)
-    with open("kalanlar.txt","w",encoding="utf-8") as file4:
-        for i in kalanlar:
-            file4.write(i)
+        with open("bsk.txt","w",encoding="utf-8") as bstas:
+            for i in bsk:
+                bstas.write(i)
+
+
